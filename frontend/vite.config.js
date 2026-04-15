dc0477b02573c7ad879800be25f75683d2f6a3ac
Shubham Kumar <kumarshubham0950@gmail.com>
2026-04-15 11:32:38 +0530

    Configure server proxy for API in Vite config
    Added server proxy configuration for API requests.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server: {
+  plugins: [react(), tailwindcss()],
+  base: '/',
+  build: {
+    chunkSizeWarningLimit: 1000,
   },
-  
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5000' // change 5000 to your backend port
+    }
+  }
 })
